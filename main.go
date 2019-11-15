a2f766ec00885732cf1e00e765a6b1cf0b38cbed
Antonio Robles <teleriloves@gmail.com>
2019-11-15 21:52:10 +0100

    Adds first iteration

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "os"
+    tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+    var (
+        port      = os.Getenv("PORT")       // sets automatically
+        publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+        token     = os.Getenv("TOKEN")      // you must add it to your config vars
+    )
+
+    webhook := &tb.Webhook{
+        Listen:   ":" + port,
+        Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+    }
+
+    pref := tb.Settings{
+        Token:  token,
+        Poller: webhook,
+    }
+
+    b, err := tb.NewBot(pref)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    b.Handle("/hello", func(m *tb.Message) {
+        b.Send(m.Sender, "Hi!")
+    })
+
+    b.Handle("/auth", func(m *tb.Message) {
+        Auth()
+        b.Send(m.Sender, "Authentificating...")
+    })
+
+    b.Handle("/hello", func(m *tb.Message) {
+        b.Send(m.Sender, "You entered "+m.Text)
+    })
+
+}
